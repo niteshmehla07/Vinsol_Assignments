ec8b17e9042a55e5494484730d178c6016433e9a
niteshmehla07 <nitesh@vinsol.com>
2018-10-17 13:38:54 +0530

    Tanmay review changes

--- JavaScript_Assignments/Assignment_5/promptActivity.js
+++ JavaScript_Assignments/Assignment_5/promptActivity.js
@@ -1,82 +1,50 @@
 //Constructor to initialize firstname and lastname
-function formConstructor (firstName, secondName, textId) {
+function PromptClass (firstName, secondName, textId) {
   this.firstName = firstName;
   this.secondName = secondName;
   this.textId = textId;
   this.textIdValue = document.getElementById(this.textId);
 }
 
-//Method of formConstructor to alert and display result
-formConstructor.prototype.alertValue = function() {
+//Method of PromptClass to get first name of user from prompt
+PromptClass.prototype.getFirstName = function() {
+  this.firstName = prompt("Enter first name: ","");
+  if(this.firstName == null) {
+    this.getFirstName();
+  } else {
+    this.firstName = this.firstName.trim();
+    if(this.firstName == "") {
+      this.getFirstName();
+    }
+  }
+}
+
+//Method of PromptClass to get last name of user from prompt
+PromptClass.prototype.getSecondName = function() {
+  this.secondName = prompt("Enter last name: ","");
+  if(this.secondName == null) {
+    this.getSecondName();
+  } else {
+    this.secondName = this.secondName.trim();
+    if(this.secondName == "")
+      this.getSecondName();
+  }
+}
+
+//Method of PromptClass to alert and display result
+PromptClass.prototype.showName = function() {
   alert("Hello " + this.firstName + " " + this.secondName);
   this.textIdValue.innerHTML = "Hello " + this.firstName + " " + this.secondName;
 }
 
 window.onload = function() {
-  var firstName = null;
-  var secondName = null;
-
-  //Function to get value of Firstname and Lastname
-  getFirstName();
-  getSecondName();
+  var firstName = null,
+      secondName = null;
 
   //Object of formConstructor
-  var name = new formConstructor(firstName, secondName, 'textOutput');
-  name.alertValue();
-
-  //Function definition to get first name
-  function getFirstName() {
-    firstName = prompt("Enter first name: ","");
-    if(firstName == null)
-      getFirstName();
-    else {
-      firstName = firstName.trim();
-      if(firstName == "")
-        getFirstName();
-    }
-  }
-
-  //Function definition to get first name
-  function getSecondName() {
-    secondName = prompt("Enter last name: ","");
-    if(secondName == null)
-      getSecondName();
-    else {
-      secondName = secondName.trim();
-      if(secondName == "")
-        getSecondName();
-    }
-  }
+  var name = new PromptClass(firstName, secondName, 'textOutput');
+  name.getFirstName();
+  name.getSecondName();
+  name.showName();
 
 }
-
-/*function getUsername() {
-  var firstName = null;
-  var secondName = null;
-  getFirstName();
-  getSecondName();
-  alert("Hello " + firstName + " " + secondName);
-  document.getElementById('textOutput').innerHTML = "Hello " + firstName + " " + secondName;
-
-  function getFirstName() {
-    firstName = prompt("Enter first name: ","");
-    if(firstName == null)
-      getFirstName();
-    else {
-      firstName = firstName.trim();
-      if(firstName == "")
-        getFirstName();
-    }
-  }
-  function getSecondName() {
-    secondName = prompt("Enter last name: ","");
-    if(secondName == null)
-      getSecondName();
-    else {
-      secondName = secondName.trim();
-      if(secondName == "")
-        getFgetSecondNameirstName();
-    }
-  }
-}
-*/
