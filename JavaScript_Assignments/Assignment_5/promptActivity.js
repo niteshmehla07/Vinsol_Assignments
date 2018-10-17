e13cc33280fc44ac95f5a5167cd5ce1dbdfcdccc
niteshmehla07 <nitesh@vinsol.com>
2018-10-17 13:07:02 +0530

    Aishmita Review Changes

--- JavaScript_Assignments/Assignment_5/promptActivity.js
+++ JavaScript_Assignments/Assignment_5/promptActivity.js
@@ -1,4 +1,56 @@
-function getUsername() {
+//Constructor to initialize firstname and lastname
+function formConstructor (firstName, secondName, textId) {
+  this.firstName = firstName;
+  this.secondName = secondName;
+  this.textId = textId;
+  this.textIdValue = document.getElementById(this.textId);
+}
+
+//Method of formConstructor to alert and display result
+formConstructor.prototype.alertValue = function() {
+  alert("Hello " + this.firstName + " " + this.secondName);
+  this.textIdValue.innerHTML = "Hello " + this.firstName + " " + this.secondName;
+}
+
+window.onload = function() {
+  var firstName = null;
+  var secondName = null;
+
+  //Function to get value of Firstname and Lastname
+  getFirstName();
+  getSecondName();
+
+  //Object of formConstructor
+  var name = new formConstructor(firstName, secondName, 'textOutput');
+  name.alertValue();
+
+  //Function definition to get first name
+  function getFirstName() {
+    firstName = prompt("Enter first name: ","");
+    if(firstName == null)
+      getFirstName();
+    else {
+      firstName = firstName.trim();
+      if(firstName == "")
+        getFirstName();
+    }
+  }
+
+  //Function definition to get first name
+  function getSecondName() {
+    secondName = prompt("Enter last name: ","");
+    if(secondName == null)
+      getSecondName();
+    else {
+      secondName = secondName.trim();
+      if(secondName == "")
+        getSecondName();
+    }
+  }
+
+}
+
+/*function getUsername() {
   var firstName = null;
   var secondName = null;
   getFirstName();
@@ -27,3 +79,4 @@ function getUsername() {
     }
   }
 }
+*/
